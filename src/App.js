96464a1f0a6019b44e2656666a2a431759b9d029
armand <armand@macbook-pro-darmand.home>
2020-01-25 22:38:15 +0100

    Feat: likes, create, login, filters

--- src/App.js
+++ src/App.js
@@ -1,27 +1,63 @@
-import React, { Suspense } from "react"
+import React, { Suspense, useEffect } from "react"
 import { Route, Switch, Redirect, withRouter } from "react-router-dom"
+import * as actions from "./features/containers/Auth"
 
 import Layout from "./features/layout/layout"
+import Home from "./features/containers/Home/components/home"
+import Login from "./features/containers/Auth/components/login"
+import Register from "./features/containers/Auth/components/register"
+import Likes from "./features/containers/Likes/components/likes"
+import Create from "./features/containers/Create/components/create"
+import Logout from "./features/containers/Auth/components/logout"
 
 import "./App.css"
+import { connect } from "react-redux"
 
 // Lazy imports containers
-const Home = React.lazy(() => {
-  return import("./features/containers/Home/components/home")
-})
-const Login = React.lazy(() => {
-  return import("./features/containers/Auth/login")
-})
+// const Login = React.lazy(() => {
+//   return import("./features/containers/Auth/components/login")
+// })
+// const Register = React.lazy(() => {
+//   return import("./features/containers/Auth/components/register")
+// })
+// const Likes = React.lazy(() => {
+//   return import("./features/containers/Likes/components/likes")
+// })
+// const Create = React.lazy(() => {
+//   return import("./features/containers/Create/components/create")
+// })
+// const Settings = React.lazy(() => {
+//   return import("./features/containers/Settings/components/settings")
+// })
 
-const App = () => {
+const App = props => {
+  const { onTryAutoSignup } = props
+
+  useEffect(() => {
+    onTryAutoSignup()
+  }, [onTryAutoSignup])
+
   //No auth routes
-  const routes = (
+  let routes = (
     <Switch>
-      <Route path="/auth" render={props => <Login {...props} />} />
+      <Route path="/register" render={props => <Register {...props} />} />
+      <Route path="/login" render={props => <Login {...props} />} />
       <Route path="/" exact component={Home} />
       <Redirect to="/" />
     </Switch>
   )
+
+  if (props.isAuthenticated) {
+    routes = (
+      <Switch>
+        <Route path="/logout" render={props => <Logout {...props} />} />
+        <Route path="/likes" render={props => <Likes {...props} />} />
+        <Route path="/create" render={props => <Create {...props} />} />
+        <Route path="/" exact component={Home} />
+        <Redirect to="/" />
+      </Switch>
+    )
+  }
 
   return (
     <>
@@ -32,4 +68,16 @@ const App = () => {
   )
 }
 
-export default withRouter(App)
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.token !== null
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onTryAutoSignup: () => dispatch(actions.authCheckState())
+  }
+}
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App))
